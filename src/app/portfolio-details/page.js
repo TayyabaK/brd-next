65ba7523948ae63872482fee656fbb0fda862c79
Tayyaba Khalil <tayyaba.khalil@gmail.com>
2024-06-02 18:17:10 +0300

    converting portfolio details to next js

--- src/app/portfolio-details/page.js
+++ src/app/portfolio-details/page.js
@@ -1,33 +1,9 @@
-'use client';
-
-import { useEffect, useState } from "react";
 import PortfolioDetailsContent from "@/elements/portfolio2/PortfolioDetailsContent";
 import PortfolioData from "@/app/data/PortfolioData.json";
-
 import Head from 'next/head';
 import Layout from "../brdLayout/brdLayout";
-import { useRouter } from "next/router";
 
-const PortfolioDetails = () => {
-    const router = useRouter();
-    const { id } = router.query;
-    const [isClient, setIsClient] = useState(false);
-
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
-
-    if (!isClient) {
-        return <div>Loading...</div>;
-    }
-
-    if (!id) {
-        return <div>Loading...</div>;
-    }
-
-    const portfolioId = parseInt(id, 10);
-    const data = PortfolioData.find(portfolio => portfolio.id === portfolioId);
-
+const PortfolioDetails = ({ data }) => {
     if (!data) {
         return <div>Page not found</div>;
     }
@@ -42,6 +18,24 @@ const PortfolioDetails = () => {
             </Layout>
         </>
     );
+};
+
+export async function getServerSideProps(context) {
+    const { id } = context.params;
+    const portfolioId = parseInt(id, 10);
+    const data = PortfolioData.find(portfolio => portfolio.id === portfolioId);
+
+    if (!data) {
+        return {
+            notFound: true,
+        };
+    }
+
+    return {
+        props: {
+            data,
+        },
+    };
 }
 
 export default PortfolioDetails;
